32b36edd4d4d18f872ca077f2727bf9241a0d691
Shane123Reed <shane@cinegrain.com>
2013-10-28 16:18:31 -0700

    savepoint

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,8 @@ class UsersController < ApplicationController
 
   def create
   	@users = User.create(params[:user].permit(:email, :password, :password_confirmation))
-  	redirect_to :action => "new"
+  	# redirect_to :action => "new"
+    redirect_to images_url
   end
 
   def destroy
